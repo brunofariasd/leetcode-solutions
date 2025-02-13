9f6d0a02172c8b102f5c540a15bfbe9e930a126c
Bruno Farias <bruno_fariasd@hotmail.com>
2025-02-13 14:44:27 -0300

    add: partial resolution

---
+++ threeSum.ts
@@ -0,0 +1,28 @@
+function threeSum(nums: number[]): number[][] {
+  if (nums.length === 3) {
+    return nums.reduce((prev, current) => prev + current) === 0 ? [nums] : [];
+  }
+
+  const hashObj = {};
+  const result: number[][] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    hashObj[nums[i]] = nums[i];
+  }
+
+  if (Object.keys(hashObj).length === 1 && hashObj[0] * 3 === 0)
+    return [Array(3).fill(nums[0])];
+
+  Object.keys(hashObj).reduce((prev, current) => {
+    const sumTwoFirst = parseInt(prev) + parseInt(current);
+    const thirdTerm = hashObj[sumTwoFirst >= 0 ? -sumTwoFirst : sumTwoFirst];
+
+    if (thirdTerm !== undefined) {
+      result.push([prev, current, thirdTerm].map((num) => parseInt(num)));
+    }
+
+    return current;
+  });
+
+  return result;
+}
