9dea3b3c9cf57b8908ee30581261337becb2f48f
Bruno Farias <bruno_fariasd@hotmail.com>
2025-03-11 11:23:35 -0300

    add: new resolution

--- binarySearch.ts
+++ binarySearch.ts
@@ -1,16 +1,15 @@
 function search(nums: number[], target: number): number {
   let startI = 0;
-  let endI = nums.length;
+  let endI = nums.length - 1;
 
-  while (nums.length > 1) {
-    const halfI = Math.floor((nums.length - 1) / 2);
+  while (startI <= endI) {
+    const halfI = startI + Math.floor((endI - startI) / 2);
 
-    if (nums[halfI] === target) return -1;
+    if (nums[halfI] === target) return halfI;
 
     if (nums[halfI] > target) {
-      nums = nums.splice(startI, halfI - 1);
+      endI = halfI - 1;
     } else {
-      nums = nums.splice(halfI + 1, nums.length - 1);
       startI = halfI + 1;
     }
   }
