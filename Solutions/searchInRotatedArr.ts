8f57b174ef58421d784e6746b77958fbfb5e7947
Bruno Farias <bruno_fariasd@hotmail.com>
2025-03-17 12:12:12 -0300

    add: new resolution
    review it again

--- Solutions/searchInRotatedArr.ts
+++ Solutions/searchInRotatedArr.ts
@@ -1,20 +1,28 @@
+//REVIEW IT AGAIN
+
 function search(nums: number[], target: number): number {
   let startI = 0;
   let endI = nums.length - 1;
 
   while (startI <= endI) {
-    let halfI = startI + Math.floor((endI - startI) / 2);
+    let halfI = Math.floor((endI + startI) / 2);
 
     if (target === nums[halfI]) return halfI;
 
-    if (target <= nums[endI] && nums[halfI] > nums[endI]) {
-      startI = halfI + 1;
+    if (nums[startI] <= nums[halfI]) {
+      if (target > nums[halfI] || target < nums[startI]) {
+        startI = halfI + 1;
+      } else {
+        endI = halfI - 1;
+      }
     } else {
-      endI = halfI - 1;
+      if (target < nums[halfI] || target > nums[endI]) {
+        endI = halfI - 1;
+      } else {
+        startI = halfI + 1;
+      }
     }
   }
 
-  return nums[startI] === target ? startI : -1;
+  return -1;
 }
-
-(0)[(4, 5, 6, 7, 0, 1, 2)];
