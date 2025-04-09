aa3643dc2541f439640a38db81875ae7cf621b79
Bruno Farias <bruno_fariasd@hotmail.com>
2025-04-09 11:55:28 -0300

    add: new resolution
    review it again

--- Solutions/permutationInString.ts
+++ Solutions/permutationInString.ts
@@ -1,31 +1,37 @@
-// Funny question
+// Funny question. Review this code again.
 
 function checkInclusion(s1: string, s2: string): boolean {
-  let rPoint = 0;
-  let increment = s2.length;
-
+  //Frequency
   let hashS1 = new Map();
   for (let i = 0; i < s1.length; i++) {
     hashS1.set(s1[i], (hashS1.get(s1[i]) || 0) + 1);
   }
 
+  let isValid = true;
   let hashS2 = new Map();
-  let lAux = 0;
-  for (let lPoint = 0; lPoint < increment; lPoint++) {
-    rPoint = lPoint + (s1.length - 1);
-    if (rPoint > s2.length - 1) return false;
 
+  let lAux = 0;
+  let lPoint = 0;
+  let rPoint = lPoint + (s1.length - 1);
+
+  while (rPoint <= s2.length - 1) {
     lAux = lPoint;
     hashS2.clear();
     while (lAux <= rPoint) {
       hashS2.set(s2[lAux], (hashS2.get(s2[lAux]) || 0) + 1);
-
-      if (hashS2.get(s2[lAux]) !== hashS1.get(s2[lAux])) lAux = rPoint + 1;
-
-      if (lAux === rPoint) return true;
-
       lAux++;
     }
+
+    isValid = true;
+    hashS1.forEach((value, key) => {
+      if (hashS2.get(key) !== value && rPoint <= s2.length - 1 && isValid) {
+        isValid = false;
+      }
+    });
+    if (isValid) return true;
+
+    lPoint++;
+    rPoint = lPoint + (s1.length - 1);
   }
 
   return false;
