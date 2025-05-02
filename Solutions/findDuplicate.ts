e7f046ab001316c310bd226fef2fe1944c8d5ce6
Bruno Farias <tbmfarias@rd.com.br>
2025-05-02 09:21:27 -0300

    add: new resolution

---
+++ Solutions/findDuplicate.ts
@@ -0,0 +1,20 @@
+/**
+ *
+ * @param nums
+ * @returns
+ *
+ * Funny question.
+ *
+ */
+
+function findDuplicate(nums: number[]): number {
+  const auxArr = new Array(nums.length + 1).fill(0);
+
+  for (let i = 0; i <= nums.length; i++) {
+    auxArr[nums[i]] += 1;
+
+    if (auxArr[nums[i]] === 2) return nums[i];
+  }
+
+  return 0;
+}
