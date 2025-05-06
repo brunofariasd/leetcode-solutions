fbf0397b9c80cae2fea0529c85cde02e3098565a
Bruno Farias <tbmfarias@rd.com.br>
2025-05-06 11:07:58 -0300

    add: new resolution

---
+++ Solutions/invertTree.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (!root) return null;
+
+  const tempLeft = root.left;
+  const tempRight = root.right;
+
+  root.left = tempRight;
+  root.right = tempLeft;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+}
