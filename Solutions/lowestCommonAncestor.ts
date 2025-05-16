5e59e27cb878de82f7c1dd3b951d287e55bfa81b
Bruno Farias <bruno_fariasd@hotmail.com>
2025-05-16 17:20:20 -0300

    add: new resolution

---
+++ Solutions/lowestCommonAncestor.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ *
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null
+): TreeNode | null {
+  if (!root || !p || !q) return null;
+
+  if (root.val > Math.max(p.val, q.val)) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (root.val < Math.min(p.val, q.val)) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+}
